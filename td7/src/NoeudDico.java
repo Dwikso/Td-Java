a9afdabed26f74f8e7e4bcba82848be64521e19b
Dwikso <quentin.pellosse2004@gmail.com>
2025-03-27 16:00:46 +0100

    feat(bgogçuzgouabgouabgouagouaiga): aougfoaubgouazgbuagbaougbazug

--- td7/src/NoeudDico.java
+++ td7/src/NoeudDico.java
@@ -1,5 +1,7 @@
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class NoeudDico {
   private boolean estFinal;
@@ -23,7 +25,57 @@ public class NoeudDico {
     return !lesSuivants.isEmpty();
   }
 
-  public ajouteUnMot(String mot) {
+  public void ajouteUnMot(String mot) {
+    NoeudDico noeudCourant = this;
+
+    for (int i = 0; i < mot.length(); i++) {
+      char caractere = mot.charAt(i);
+
+      if (!noeudCourant.lesSuivants.containsKey(caractere)) {
+        noeudCourant.lesSuivants.put(caractere, new NoeudDico());
+      }
+
+      noeudCourant = noeudCourant.lesSuivants.get(caractere);
+    }
+
+    noeudCourant.estFinal = true;
+  }
+
+  public boolean supprimerMot(String mot) {
+    NoeudDico noeudCourant = this;
+    List<NoeudDico> chemin = new ArrayList<>();
+
+    for (char c : mot.toCharArray()) {
+      if (!noeudCourant.lesSuivants.containsKey(c)) {
+        return false;
+      }
+      chemin.add(noeudCourant);
+      noeudCourant = noeudCourant.lesSuivants.get(c);
+    }
+    if (!noeudCourant.estFinMot()) {
+      return false;
+    }
+    noeudCourant.nbre--;
+    if (noeudCourant.nbre == 0) {
+      noeudCourant.estFinal = false;
+    }
+    return true;
+  }
+
+  public int nombreOccurences(String mot) {
+    NoeudDico noeudCourant = this;
+    List<NoeudDico> chemin = new ArrayList<>();
+
+    for (char c : mot.toCharArray()) {
+      if (!noeudCourant.lesSuivants.containsKey(c)) {
+        return 0;
+      }
+      noeudCourant = noeudCourant.lesSuivants.get(c);
+    }
+    if (!noeudCourant.estFinMot()) {
+      return 0;
+    }
+    return noeudCourant.nbre;
   }
 
 }
